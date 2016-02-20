31d76b3d09d87e1ae398ccbe87338d715300cb99
Aaron Wilson <atw1394@gmail.com>
2016-02-20 15:41:32 -0500

    Added files via upload

---
+++ AboutMe.py
@@ -0,0 +1,11 @@
+name = 'Aaron'
+age = 21
+weight = 195
+height = 69
+
+print "I'd like to talk about myself for a moment."
+print "My name is %s" % name
+print "I am %d years old." % age
+print "I am %d pounds, which isn't too heavy I would guess." % weight
+print "And I am %d inches tall. Which is about average." % height
+
---
+++ SimpleStuff.py
@@ -0,0 +1,3 @@
+print "I know that simple math can be done jsut by typing is out."
+print "For example I'll do 1 + 1."
+print "That equals", 1 + 1
