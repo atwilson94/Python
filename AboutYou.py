31068229ca7c4a76707577477ab723295ddd703b
Aaron Wilson <atw1394@gmail.com>
2016-02-20 16:01:14 -0500

    Added files via upload

---
+++ AboutYou.py
@@ -0,0 +1,9 @@
+print "Now let's get some info about you."
+print "What is your name?",
+name = raw_input()
+print "Where are you from?",
+birthplace = raw_input()
+print "How old are you?",
+age = raw_input()
+print "Alright, let's review what you said."
+print "You said your name is %s. \nYou are from %s. \nAnd you are %s years old." % (name, birthplace, age) 
