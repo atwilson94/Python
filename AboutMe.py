c9a425e6a8d68ed60e0a382e723d3a04e116e7d9
Aaron Wilson <atw1394@gmail.com>
2016-02-20 16:00:41 -0500

    Update AboutMe.py

--- AboutMe.py
+++ AboutMe.py
@@ -4,7 +4,7 @@ weight = 195
 height = 69
 
 print "I'd like to talk about myself for a moment."
-print "My name is %s" % name
+print "My name is %s." % name
 print "I am %d years old." % age
 print "I am %d pounds, which isn't too heavy I would guess." % weight
 print "And I am %d inches tall. Which is about average." % height
